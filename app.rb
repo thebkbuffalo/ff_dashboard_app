225d9775ba84d2a12849ba03bfea28e327431651
Evan Berg <evan.m.berg@gmail.com>
2014-09-06 00:03:09 -0400

    beginning stages set up, basic index page and forum for feed selection begun. communication with Redis maybe working.

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require 'sinatra/base'
+require 'json'
+require 'pry'
+require 'httparty'
+require 'redis'
+require 'securerandom'
+
 
 class App < Sinatra::Base
 
@@ -10,6 +16,11 @@ class App < Sinatra::Base
     enable :logging
     enable :method_override
     enable :sessions
+    set :session_secret, 'super secret'
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    $redis = Redis.new({:host => uri.host,
+                        :port => uri.port,
+                        :password => uri.password})
   end
 
   before do
@@ -22,10 +33,63 @@ class App < Sinatra::Base
   end
 
   ########################
-  # Routes
+  # API Keys
+  #######################
+
+  KEY = "53bbd3a7cce7cbbcedc67e22259f12ee:4:69767384"
+  CALLBACK_URI = "http://127.0.0.1/profile"
+
+
+  ########################
+  # GET Routes
   ########################
 
   get('/') do
+
     render(:erb, :index)
   end
+
+  get('/profile') do
+    render(:erb, :profile)
+  end
+
+  get('/dashboard_form') do
+    render(:erb, :dashboard_form)
+  end
+
+  # get('/profile') do
+  #   base_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json"
+  #   query = "football"
+  #   begin_date = 20140904
+  #   end_date = 20141131
+  #   @url = "#{base_url}?q=#{query}&begin_date=#{begin_date}&end_date=#{end_date}&api-key=#{KEY}&callback=#{CALLBACK_URI}"
+  #   response = HTTParty.get
+  #   render(:erb, :profile)
+  # end
+
+  ##################################
+  # POST Routes
+  ##################################
+
+  post('/dashboard_form') do
+    new_feed = {
+      feeds: params["feeds"]
+    }
+
+    post_to_redis("feeds")
+
+    redirect to('/dashboard_form')
+  end
+
+  ###################################
+  # REDIS Methods
+  ###################################
+
+  def post_to_redis(feeds)
+    key = "source"
+    value = "feeds"
+    $redis.set("key", "value" )
+  end
+
+
 end # ends class
--- readme.md
+++ readme.md
@@ -1,1 +1,35 @@
 # ff_dashboard_app
+
+# USER STORIES ----------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+As a User, I can Log Into my Profile
+As a User, I can Pick and Choose what RSS feeds/API's I want
+---Options will include:
+  -ESPN RSS Feed
+  -Bleacher Report Fantasy RSS Feed
+  -Twitter Fantasy Football Feed
+  -Matthew Berry Twitter Feed
+  -RotoWire RSS Feed
+  -Yahoo Football RSS Feed
+  -Football Guys RSS Feed
+
+As a User, I can go to my individual Dashboard
+As a User, I can go to an edit page for my Dashboard
+As a User, I can delete RSS feeds from my Dashboard
